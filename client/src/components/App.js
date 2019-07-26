cbacd9af7e9479940c035f48f185a4b2b0f82002
Sandeep Thapa <sandeep.thapa@outlook.com>
2019-07-26 12:01:24 +0300

    Added boilerplate routing for client side streaming app

---
+++ client/src/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+
+
+
+
+const App = () => {
+
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={StreamList}></Route>
+                    <Route path="/streams/new" component={StreamCreate}></Route>
+                    <Route path="/streams/edit" component={StreamEdit}></Route>
+                    <Route path="/streams/delete" component={StreamDelete}></Route>
+                    <Route path="/streams/show" component={StreamShow}></Route>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
