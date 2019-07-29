3c98a4ea73ce7ed38811942f119f9da0e9deeabe
Sandeep Thapa <sandeep.thapa@outlook.com>
2019-07-29 14:36:58 +0300

    Added redux form and basic error handling

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
+class StreamCreate extends React.Component {
+    renderInput({ input, label, meta }) {
 
-    return (
-        <div>Create </div>
-    )
+        return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} />
+                <div>{meta.error}</div>
+            </div>
+        );
+    }
+
+    onSubmit(formData) {
+
+    }
+    render() {
+        return (
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+                <Field name="title" component={this.renderInput} label="Enter Title" />
+                <Field name="description" component={this.renderInput} label="Enter Description" />
+                <button className="ui primary button" >Submit</button>
+            </form>
+        );
+    }
 }
 
-export default StreamCreate;
+const validate = (formValues) => {
+    const errors = {};
+    if (!formValues.title) {
+        errors.title = "Please enter title of stream"
+    }
+    if (!formValues.description) {
+        errors.description = "Please enter description for stream"
+    }
+
+    return errors;
+
+}
+
+export default reduxForm({
+    form: 'streamCreate',
+    validate
+})(StreamCreate);
--- client/src/index.js
+++ client/src/index.js
@@ -9,11 +9,11 @@ import reducers from './reducers';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     reducers,
-    composeEnhancers(applyMiddleware()
-    ));
+    composeEnhancers(applyMiddleware())
+);
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
-    </Provider>
-    , document.querySelector('#root'));
+    </Provider>,
+    document.querySelector('#root'));
