c8ecac8d02ac66eff62de922f3b458447113c4fd
Sandeep Thapa <sandeep.thapa@outlook.com>
2019-07-29 10:44:01 +0300

    Google auth functionality imporvement

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -19,16 +19,34 @@ class GoogleAuth extends React.Component {
         });
     }
     onAuthChange = () => {
-
-        this.setState({ isSignedIn: true })
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
     }
+
+    onSignIn = () => {
+        this.auth.signIn();
+    }
+    onSignOut = () => {
+        this.auth.signOut();
+    }
+
+
     renderAuthButton() {
         if (this.state.isSignedIn === null) {
-            return <div>No information on authentication</div>;
+            return null;
         } else if (this.state.isSignedIn) {
-            return <div>Signed In</div>
+            return (
+                <button onClick={this.onSignOut} className="ui red google button">
+                    <i className="google icon" />
+                    Sign Out
+          </button>
+            );
         } else {
-            return <div>Not signed in</div>
+            return (
+                <button onClick={this.onSignIn} className="ui red google button">
+                    <i className="google icon" />
+                    Sign In
+          </button>
+            );
         }
 
     }
