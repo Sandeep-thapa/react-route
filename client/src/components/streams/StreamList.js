0b52de02ca53d92a4707dd430268dc0b8075af00
Sandeep Thapa <sandeep.thapa@outlook.com>
2019-07-30 15:59:59 +0300

    added delete stream functionalitiees

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -14,6 +14,9 @@ class StreamList extends React.Component {
                     <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
                         Edit
                         </Link>
+                    <Link to={`/streams/delete/${stream.id}`} className="ui button negative">
+                        Delete
+                        </Link>
 
 
 
