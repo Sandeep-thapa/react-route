7b4444854ac3d79772ed54e1192237988e609622
Sandeep Thapa <sandeep.thapa@outlook.com>
2019-08-04 14:39:06 +0300

    Completion opf project for streaming replica

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -48,6 +48,8 @@ export const fetchStream = (id) => async dispatch => {
     dispatch({ type: FETCH_STREAM, payload: response.data })
 
 };
+
+
 export const editStream = (id, formValues) => async dispatch => {
     const response = await streams.patch(`/streams/${id}`, formValues);
 
